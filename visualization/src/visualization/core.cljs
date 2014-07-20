cee21521fd35c2450a0eeb9b715b49dfd0e7c60d
Bert Muthalaly <bgmuthalaly@gmail.com>
2014-07-20 04:00:53 -0400

    Draw chart frame

--- visualization/src/visualization/core.cljs
+++ visualization/src/visualization/core.cljs
@@ -5,6 +5,12 @@
 (defn chart-frame [x y size]
   (-> js/document 
       (.getElementById "container")
-      (aset "textContent" "whaat")))
+      (aset "innerHTML" "<div id='chart'></div>"))
+  (-> (d3/select "div#chart")
+      (.style "border-left" "solid black")
+      (.style "border-bottom" "solid black")
+      (.style "width" (str size))
+      (.style "height" (str size))))
+      
 
-(.log js/console (chart-frame nil nil nil))
+(.log js/console (chart-frame nil nil 200))
